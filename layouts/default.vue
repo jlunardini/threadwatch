a8a09fd1b4cc287c560b1e287b20fd9abef741aa
Johncarlos Lunardini <jlunardini@Johncarloss-MacBook-Air.local>
2021-04-15 23:03:43 -0400

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="grid grid-cols-12">
+    <div class="bg-gray-100 col-span-1 h-screen">test</div>
+    <div class="col-span-11">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<style>
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
